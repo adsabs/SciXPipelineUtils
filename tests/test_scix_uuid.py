36602c058ef824f1ceac120cd8c3f3d1daf469ad
tjacovich <tjacovich@cfa.harvard.edu>
2023-11-01 15:57:53 -0400

    Add uuid7 to PipelineUtils.

---
+++ tests/test_scix_uuid.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+import scix_uuid
+import uuid
+
+class TestSciXUUIDImplementation(TestCase):
+    def generate_uuid7(self):
+        test_uuid = scix_uuid.uuid7()
+        self.assertEqual(type(test_uuid), uuid.UUID)
+        self.assertEqual(type(test_uuid.hex), str)
+        self.assertEqual(len(test_uuid.bytes), 16)
