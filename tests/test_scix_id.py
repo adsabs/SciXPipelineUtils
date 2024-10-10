dc70e899e302678a53777358177aab6d041f330a
tjacovich <tjacovich@cfa.harvard.edu>
2024-10-10 14:18:26 -0400

    Fixed test names. Added No checksum test.

--- tests/test_scix_id.py
+++ tests/test_scix_id.py
@@ -3,9 +3,15 @@ from unittest import TestCase
 import SciXPipelineUtils.scix_id as scixid
 
 
-class TestSciXUUIDImplementation(TestCase):
-    def test_generate_uuid7(self):
+class TestSciXIDImplementation(TestCase):
+    def test_generate_scixid(self):
         test_id = scixid.encode(1000)
         self.assertEqual(test_id, "0000-0000-0Z81")
         test_int = scixid.decode(test_id)
+        self.assertEqual(test_int, 1000)
+
+    def test_generate_scixid_no_checksum(self):
+        test_id = scixid.encode(1000, checksum=False)
+        self.assertEqual(test_id, "0000-0000-00Z8")
+        test_int = scixid.decode(test_id, checksum=False)
         self.assertEqual(test_int, 1000)
