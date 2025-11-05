86d5988bd77179f7b6db94757e765ae1c06bbf36
tjacovich <tjacovich@cfa.harvard.edu>
2025-11-05 16:46:59 -0500

    Commit linter changes.

--- SciXPipelineUtils/scix_id.py
+++ SciXPipelineUtils/scix_id.py
@@ -208,14 +208,16 @@ def generate_bib_data_hash(hash_data, strip_characters=True, user_fields=None):
         "date",
         "copyright",
     ]
+
     hash_data_fields = list(hash_data.keys())
+
     if user_fields:
         for field in hash_data_fields:
             if field not in user_fields:
-              try:
-                  hash_data.pop(field)
-              except Exception:
-                  continue      
+                try:
+                    hash_data.pop(field)
+                except Exception:
+                    continue
     else:
         for field in unique_fields:
             try:
